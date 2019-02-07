d0490621d35ed5cee78c391707e2ffb450f1197b
alexboboc <me@alexboboc.com>
2019-02-07 03:52:09 +0100

    Commented for ease of maintenance

--- background.js
+++ background.js
@@ -1,3 +1,4 @@
+// Inject JavaScript into current website
 chrome.browserAction.onClicked.addListener(function (tab) {
 	chrome.tabs.executeScript(tab.ib, {
 		file: 'logic.js'
--- logic.js
+++ logic.js
@@ -4,7 +4,8 @@
     var currentElementHovered;
 
     function createCanvas() {
-        canvasContainer = document.createElement('div');
+        // Create div covering 100% of the page and place on top
+        var canvasContainer = document.createElement('div');
         canvasContainer.id = "extensionCanvasTemporary";
         document.body.appendChild(canvasContainer);
         canvasContainer.style.position = "absolute";
@@ -13,20 +14,25 @@
         canvasContainer.style.width = "100%";
         canvasContainer.style.height = "100%";
         canvasContainer.style.zIndex = "100000";
-        superContainer = document.body;
-        canvas = document.createElement('canvas');
-        canvas.style.width = superContainer.scrollWidth+"px";
-        canvas.style.height = superContainer.scrollHeight+"px";
+        
+        // Create canvas filling the whole div
+        var superContainer = document.body;
+        canvas = document.createElement("canvas");
+        canvas.style.width = superContainer.scrollWidth + "px";
+        canvas.style.height = superContainer.scrollHeight + "px";
         canvas.width = superContainer.scrollWidth;
         canvas.height = superContainer.scrollHeight;
-        canvas.style.overflow = 'visible';
-        canvas.style.position = 'absolute';
-        var context = canvas.getContext('2d');
+        canvas.style.overflow = "visible";
+        canvas.style.position = "absolute";
+        var context = canvas.getContext("2d");
         canvasContainer.appendChild(canvas);
+        
+        // Return context to be able to draw on it
         return context;
     }
 
     function getDomPath(el) {
+        // Inversely traverse the DOM tree starting at the given node, and log steps
         var stack = [];
         while (el.parentNode != null) {
             var sibCount = 0;
@@ -49,12 +55,15 @@
             }
             el = el.parentNode;
         }
+        
+        // Join all elements traversed into final path
         var pathComponents = stack.slice(1);
         return pathComponents.join(' > ');
     }
 
     function copyToClipboard(text) {
-        const el = document.createElement("textarea");
+        // Place text into clipboard by temporarily generating an invisible textarea
+        var el = document.createElement("textarea");
         el.value = text;
         el.setAttribute('readonly', '');
         el.style.position = 'absolute';
@@ -66,6 +75,7 @@
     }
 
     function removeCanvas() {
+        // Delete canvas and its container
         var canvasContainer = document.getElementById("extensionCanvasTemporary");
         if (canvasContainer) {
             canvasContainer.remove();
@@ -73,34 +83,47 @@
     }
 
     function getElementUnderMouse(e) {
+        // Check the highest element being hovered
         var x = e.pageX - window.pageXOffset;
         var y = e.pageY - window.pageYOffset;
         var currentElement = document.elementFromPoint(x, y);
         return currentElement;
     }
 
+    document.onmousemove = function(e) {
+        // Delete canvas to be able to read hovered element
+        removeCanvas();
+        
+        // Read hovered element and its bounding box
+        var rect = currentElement.getBoundingClientRect();
+        var currentElement = getElementUnderMouse(e);        
+        
+        // Create canvas and draw a red box over the hovered element
+        var context = createCanvas();
+        context.fillStyle = "rgba(255,0,0,0.5)";
+        context.fillRect(rect.x, rect.y + document.documentElement.scrollTop, rect.width, rect.height);
+        
+        currentElementHovered = currentElement;
+    }
+
 	document.onclick = function(e) {
+        // Compute path of the hovered element and save to clipboard
         var path = getDomPath(currentElementHovered);
         copyToClipboard(path);
+        
+        // Cleanup events and canvas
         document.onclick = function(e) {}
         document.onmousemove = function(e) {}
         removeCanvas();
+        
+        // Display green rectangle for 1 second as success feedback
         var context = createCanvas();
         var rect = currentElementHovered.getBoundingClientRect();
         context.fillStyle = "rgba(0,255,0,0.5)";
         context.fillRect(rect.x, rect.y + document.documentElement.scrollTop, rect.width, rect.height);
         setTimeout(removeCanvas, 1000);
+        
         console.log("Extension shut down."); 
-    }
-
-    document.onmousemove = function(e) {
-        removeCanvas();
-        var currentElement = getElementUnderMouse(e);        
-        var rect = currentElement.getBoundingClientRect();
-        var context = createCanvas();
-        context.fillStyle = "rgba(255,0,0,0.5)";
-        context.fillRect(rect.x, rect.y + document.documentElement.scrollTop, rect.width, rect.height);
-        currentElementHovered = currentElement;
     }
 
 })();
